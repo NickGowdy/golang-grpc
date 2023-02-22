7d8e7ed90450e7632960b5efb36e88e2a7ece089
Nick Gowdy <nickgowdy87@gmail.com>
2023-02-22 16:44:48 +0000

    client that uses gRPC

---
+++ client/client.go
@@ -0,0 +1,61 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/NickGowdy/golang-grpc/proto"
+	"github.com/NickGowdy/golang-grpc/types"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func NewGRPCClient(remoteAddr string) (proto.PriceFetcherClient, error) {
+	conn, err := grpc.Dial(remoteAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+
+	c := proto.NewPriceFetcherClient(conn)
+	return c, nil
+}
+
+type Client struct {
+	endpoint string
+}
+
+func New(endpoint string) *Client {
+	return &Client{
+		endpoint: endpoint,
+	}
+}
+
+func (c *Client) FetchPrice(ticker string) (*types.PriceResponse, error) {
+	endpoint := fmt.Sprintf("%s?ticker=%s", c.endpoint, ticker)
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		errResp := map[string]any{}
+		if err := json.NewDecoder(resp.Body).Decode(&errResp); err != nil {
+			return nil, err
+		}
+		return nil, fmt.Errorf("service responded with a non 200 status code: %s", errResp["error fetching price"])
+	}
+
+	priceResp := new(types.PriceResponse)
+	if err := json.NewDecoder(resp.Body).Decode(priceResp); err != nil {
+		return nil, err
+	}
+	resp.Body.Close()
+
+	return priceResp, nil
+}
--- main.go
+++ main.go
@@ -1,15 +1,41 @@
 package main
 
-import "flag"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/NickGowdy/golang-grpc/client"
+	"github.com/NickGowdy/golang-grpc/proto"
+)
 
 func main() {
 	var (
 		jsonAddr = flag.String("json", ":3000", "listen address of the json transport")
 		grpcAddr = flag.String("grpc", ":4000", "listen address of the grpc transport")
+		svc      = loggingService{priceService{}}
+		ctx      = context.Background()
 	)
 	flag.Parse()
 
-	svc := loggingService{priceService{}}
+	grpcClient, err := client.NewGRPCClient(":4000")
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		resp, err := grpcClient.FetchPrice(ctx, &proto.PriceRequest{Ticker: "BTC"})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%+v\n", resp)
+	}()
 
 	go makeGRPCServerAndRun(*grpcAddr, svc)
 
